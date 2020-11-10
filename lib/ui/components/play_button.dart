0cf468d7ebfaf5245e7e91bc02aa85ef0ef4b311
martin <martin.oblikas@concise.ee>
2020-11-10 12:46:05 +0200

    Add play button widget

---
+++ lib/ui/components/play_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class PlayButton extends StatelessWidget {
+  PlayButton({@required this.onPressed});
+
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+        fit: BoxFit.fitWidth,
+        child: IconButton(
+        icon: Icon(Icons.play_arrow),
+          padding: EdgeInsets.symmetric(horizontal: 200.0),
+          color: Colors.yellowAccent,
+          iconSize: 300.0,
+          onPressed: onPressed,
+        ));
+  }
+}
