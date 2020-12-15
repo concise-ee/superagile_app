29f8dafd80d08ef3225b11858b724c898c774134
Jakob Pindis <jakob.pindis@concise.ee>
2020-12-15 15:38:30 +0200

    SA-#41 show different text for host/player on congratulations page

--- lib/ui/views/congratulations_page.dart
+++ lib/ui/views/congratulations_page.dart
@@ -143,7 +143,7 @@ class _CongratulationsPage extends State<CongratulationsPage> {
                 alignment: Alignment.center,
                 child: Padding(
                     padding: EdgeInsets.all(12.0),
-                    child: Text(GO_TO_NEXT_QUESTION,
+                    child: Text(isHost ? GO_TO_NEXT_QUESTION : WAIT_FOR_NEXT_QUESTION,
                         style: TextStyle(color: Colors.yellowAccent, fontSize: 14, letterSpacing: 1.5),
                         textAlign: TextAlign.center)),
               ),
