c20908337101c38f39938b5b17bbda407d270102
Roosi-Ann <roosi-ann.tolgu@concise.ee>
2021-01-20 11:09:30 +0200

    SA-#54: add topic name to question template

--- lib/entities/question_template.dart
+++ lib/entities/question_template.dart
@@ -7,6 +7,7 @@ const THREE_MEANING = '3_meaning';
 const SHORT_DESC = 'short_description';
 const LONG_DESC = 'long_description';
 const QUESTION = 'question';
+const TOPIC_NAME = 'topic_name';
 
 class QuestionTemplate {
   String zeroMeaning;
@@ -16,11 +17,12 @@ class QuestionTemplate {
   String shortDesc;
   String longDesc;
   String question;
+  String topicName;
 
   DocumentReference reference;
 
   QuestionTemplate(this.zeroMeaning, this.oneMeaning, this.twoMeaning, this.threeMeaning, this.shortDesc, this.longDesc,
-      this.question);
+      this.question, this.topicName);
 
   factory QuestionTemplate.fromSnapshot(DocumentSnapshot snapshot) {
     var newQuestion = QuestionTemplate.fromJson(snapshot.data());
@@ -30,13 +32,13 @@ class QuestionTemplate {
 
   factory QuestionTemplate.fromJson(Map<String, dynamic> json) {
     return QuestionTemplate(json[ZERO_MEANING], json[ONE_MEANING], json[TWO_MEANING], json[THREE_MEANING], json[SHORT_DESC],
-        json[LONG_DESC], json[QUESTION]);
+        json[LONG_DESC], json[QUESTION], json[TOPIC_NAME]);
   }
 
   @override
   String toString() {
     return 'Question{$ZERO_MEANING: $zeroMeaning, $ONE_MEANING: $oneMeaning, $TWO_MEANING: $twoMeaning, '
         '$THREE_MEANING: $threeMeaning, $SHORT_DESC: $shortDesc, $LONG_DESC: $longDesc, $QUESTION: '
-        '$question, reference: $reference}';
+        '$question, $TOPIC_NAME: $topicName, reference: $reference}';
   }
 }
