0eee39c9e9f4cdac0404ac5ac45814dd7e4b5258
Roosi-Ann <roosi-ann.tolgu@concise.ee>
2021-01-26 19:19:52 +0200

    SA-#58 make changes to service for compatibility with new questions repository structure

--- lib/repositories/question_repository.dart
+++ lib/repositories/question_repository.dart
@@ -2,13 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:superagile_app/entities/question_template.dart';
 
 const QUESTIONS_COLLECTION = 'questions';
-const QUESTION_DOC_REFERENCE = 'Z0n947Uqvg2R3gH74kyc';
 
 class QuestionRepository {
   final CollectionReference _repository = FirebaseFirestore.instance.collection(QUESTIONS_COLLECTION);
 
   Future<QuestionTemplate> findQuestionByNumber(questionNr) async {
-    var snapshot = await _repository.doc(QUESTION_DOC_REFERENCE).collection(questionNr.toString()).get();
-    return QuestionTemplate.fromSnapshot(snapshot.docs.single);
+    var snapshot = await _repository.doc(questionNr.toString()).get();
+    return QuestionTemplate.fromSnapshot(snapshot);
   }
 }
