1f3e0f152b1eb0e423adae91e9942673ae0337c2
Jakob Pindis <jakob.pindis@concise.ee>
2021-02-25 10:11:30 +0200

    SA-#72 button and textfield style changes

---
+++ lib/ui/components/rounded_text_form_field.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:superagile_app/utils/global_theme.dart';
+
+class RoundedTextFormField extends StatelessWidget {
+  RoundedTextFormField(
+      {@required this.hintText,
+      @required this.controller,
+      this.maxLength,
+      this.keyboardType,
+      this.key,
+      this.validator});
+
+  final String hintText;
+  final TextEditingController controller;
+  final int maxLength;
+  final TextInputType keyboardType;
+  final Key key;
+  final Function(String) validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      child: TextFormField(
+        controller: controller,
+        maxLength: maxLength,
+        keyboardType: keyboardType,
+        key: key,
+        validator: validator,
+        textAlign: TextAlign.center,
+        textAlignVertical: TextAlignVertical.top,
+        style: TextStyle(fontSize: 24),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(24.0),
+          hintText: hintText,
+          hintStyle: TextStyle(fontSize: 24, color: accentColor),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: secondaryColor, width: 3), borderRadius: BorderRadius.circular(60.0)),
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: secondaryColor, width: 3), borderRadius: BorderRadius.circular(60.0)),
+        ),
+      ),
+    );
+  }
+}
